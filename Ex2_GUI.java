5eb49a0188f3aeb1275dfc8f3d903dc635ca047b
Owner <eliyasuissa040407@gmail.com>
2025-12-28 21:26:49 +0200

    added Ex2_GUI

--- Ex2_GUI.java
+++ Ex2_GUI.java
@@ -9,7 +9,22 @@
  */
 public class Ex2_GUI {
     public static void drawMap(Map2D map) {
-        //
+        int w = map.getWidth();
+        int h = map.getHeight();
+
+        StdDraw.setCanvasSize(w * 4, h * 4);
+        StdDraw.setXscale(-0.5, w - 0.5);
+        StdDraw.setYscale(-0.5, h - 0.5);
+        StdDraw.enableDoubleBuffering();
+
+        for (int x = 0; x < w; x++) {
+            for (int y = 0; y < h; y++) {
+                int colorValue = map.getPixel(x, y);
+                StdDraw.setPenColor(colorValue, colorValue, colorValue);
+                StdDraw.filledSquare(x, y, 0.5);
+            }
+        }
+        StdDraw.show();
     }
 
     /**
@@ -18,9 +33,42 @@ public class Ex2_GUI {
      */
     public static Map2D loadMap(String mapFileName) {
         Map2D ans = null;
+        try {
+            File myFile = new File(mapFileName);
+            Scanner s1 = new Scanner(myFile);
+            int rows = 0;
+            int cols = 0;
 
+            while (s1.hasNextLine()) {
+                String rowText = s1.nextLine().trim();
+                if (rowText.length() > 0) {
+                    if (rows == 0) {
+                        cols = rowText.split("\\s+").length;
+                    }
+                    rows++;
+                }
+            }
+            s1.close();
+
+            if (rows > 0 && cols > 0) {
+                ans = new Map(cols, rows, 0);
+                Scanner s2 = new Scanner(myFile);
+                for (int j = 0; j < rows; j++) {
+                    for (int i = 0; i < cols; i++) {
+                        if (s2.hasNextInt()) {
+                            ans.setPixel(i, j, s2.nextInt());
+                        }
+                    }
+                }
+                s2.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return ans;
     }
+
+}
 
     /**
      *
@@ -28,13 +76,42 @@ public class Ex2_GUI {
      * @param mapFileName
      */
     public static void saveMap(Map2D map, String mapFileName) {
-
-
+        try {
+            FileWriter myWriter = new FileWriter(mapFileName);
+            for (int j = 0; j < map.getHeight(); j++) {
+                for (int i = 0; i < map.getWidth(); i++) {
+                    // כותבים את המספר ורווח אחריו
+                    myWriter.write(map.getPixel(i, j) + " ");
+                }
+                myWriter.write("\n"); // יורדים שורה בסוף כל שורת פיקסלים
+            }
+            myWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public static void main(String[] a) {
-        String mapFile = "map.txt";
-        Map2D map = loadMap(mapFile);
-        drawMap(map);
+        int w = java.awt.Color.WHITE.getRGB();
+        int b = java.awt.Color.BLACK.getRGB();
+        int r = java.awt.Color.RED.getRGB();
+
+        int[][] maze = {
+                {b, b, b, b, b, b},
+                {b, w, w, w, r, b},
+                {b, w, b, b, w, b},
+                {b, w, w, w, w, b},
+                {b, b, b, b, b, b}
+        };
+
+        Map2D myMap = new Map(maze);
+        String fileName = "myMaze.txt";
+
+
+        saveMap(myMap, fileName);
+        Map2D loaded = Ex2_GUI.loadMap(fileName);
+        if (loaded != null) {
+            Ex2_GUI.drawMap(loaded);
+        }
     }
     /// ///////////// Private functions ///////////////
-}
+
