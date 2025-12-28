c060329f16c5bcc64e25711c9c3d0045378ac362
Owner <eliyasuissa040407@gmail.com>
2025-12-28 22:11:25 +0200

    changed Ex2_GUI

--- Ex2_GUI.java
+++ Ex2_GUI.java
@@ -1,117 +1,98 @@
-/**
- * Intro2CS_2026A
- * This class represents a Graphical User Interface (GUI) for Map2D.
- * The class has save and load functions, and a GUI draw function.
- * You should implement this class, it is recommender to use the StdDraw class, as in:
- * https://introcs.cs.princeton.edu/java/stdlib/javadoc/StdDraw.html
- *
- *
- */
+import java.awt.Color;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Ex2_GUI {
+
     public static void drawMap(Map2D map) {
         int w = map.getWidth();
         int h = map.getHeight();
 
-        StdDraw.setCanvasSize(w * 4, h * 4);
+        // הגדלת חלון התצוגה כדי שיהיה ברור
+        StdDraw.setCanvasSize(600, 400);
         StdDraw.setXscale(-0.5, w - 0.5);
         StdDraw.setYscale(-0.5, h - 0.5);
         StdDraw.enableDoubleBuffering();
 
         for (int x = 0; x < w; x++) {
             for (int y = 0; y < h; y++) {
-                int colorValue = map.getPixel(x, y);
-                StdDraw.setPenColor(colorValue, colorValue, colorValue);
+                int rgb = map.getPixel(x, y);
+                StdDraw.setPenColor(new Color(rgb));
                 StdDraw.filledSquare(x, y, 0.5);
             }
         }
         StdDraw.show();
     }
 
-    /**
-     * @param mapFileName
-     * @return
-     */
     public static Map2D loadMap(String mapFileName) {
-        Map2D ans = null;
         try {
-            File myFile = new File(mapFileName);
-            Scanner s1 = new Scanner(myFile);
-            int rows = 0;
-            int cols = 0;
+            Scanner sc = new Scanner(new File(mapFileName));
+            if (!sc.hasNextInt()) return null;
+            int w = sc.nextInt();
+            int h = sc.nextInt();
+            Map2D ans = new Map(w, h, 0);
 
-            while (s1.hasNextLine()) {
-                String rowText = s1.nextLine().trim();
-                if (rowText.length() > 0) {
-                    if (rows == 0) {
-                        cols = rowText.split("\\s+").length;
-                    }
-                    rows++;
-                }
-            }
-            s1.close();
-
-            if (rows > 0 && cols > 0) {
-                ans = new Map(cols, rows, 0);
-                Scanner s2 = new Scanner(myFile);
-                for (int j = 0; j < rows; j++) {
-                    for (int i = 0; i < cols; i++) {
-                        if (s2.hasNextInt()) {
-                            ans.setPixel(i, j, s2.nextInt());
-                        }
+            for (int y = 0; y < h; y++) {
+                for (int x = 0; x < w; x++) {
+                    if (sc.hasNextInt()) {
+                        ans.setPixel(x, y, sc.nextInt());
                     }
                 }
-                s2.close();
             }
+            sc.close();
+            return ans;
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return ans;
     }
 
-}
-
-    /**
-     *
-     * @param map
-     * @param mapFileName
-     */
     public static void saveMap(Map2D map, String mapFileName) {
         try {
             FileWriter myWriter = new FileWriter(mapFileName);
-            for (int j = 0; j < map.getHeight(); j++) {
-                for (int i = 0; i < map.getWidth(); i++) {
-                    // כותבים את המספר ורווח אחריו
-                    myWriter.write(map.getPixel(i, j) + " ");
+            myWriter.write(map.getWidth() + " " + map.getHeight() + "\n");
+
+            for (int y = 0; y < map.getHeight(); y++) {
+                for (int x = 0; x < map.getWidth(); x++) {
+                    myWriter.write(map.getPixel(x, y) + " ");
                 }
-                myWriter.write("\n"); // יורדים שורה בסוף כל שורת פיקסלים
+                myWriter.write("\n");
             }
             myWriter.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void main(String[] a) {
-        int w = java.awt.Color.WHITE.getRGB();
-        int b = java.awt.Color.BLACK.getRGB();
-        int r = java.awt.Color.RED.getRGB();
 
-        int[][] maze = {
-                {b, b, b, b, b, b},
-                {b, w, w, w, r, b},
-                {b, w, b, b, w, b},
-                {b, w, w, w, w, b},
-                {b, b, b, b, b, b}
+    public static void main(String[] a) {
+        int[] colors = {
+                Color.RED.getRGB(), Color.GREEN.getRGB(), Color.BLUE.getRGB(),
+                Color.YELLOW.getRGB(), Color.MAGENTA.getRGB(), Color.ORANGE.getRGB()
         };
 
-        Map2D myMap = new Map(maze);
-        String fileName = "myMaze.txt";
+        int width = 300;
+        int height = 200;
+        Map2D myMap = new Map(width, height, 0);
 
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int col = x / (width / 3);
+                int row = y / (height / 2);
+                int colorIndex = row * 3 + col;
 
+                if (colorIndex > 5) colorIndex = 5;
+
+                myMap.setPixel(x, y, colors[colorIndex]);
+            }
+        }
+
+        String fileName = "six_colors_map.txt";
         saveMap(myMap, fileName);
-        Map2D loaded = Ex2_GUI.loadMap(fileName);
+
+        Map2D loaded = loadMap(fileName);
         if (loaded != null) {
-            Ex2_GUI.drawMap(loaded);
+            drawMap(loaded);
         }
     }
-    /// ///////////// Private functions ///////////////
-
+}
